bfdc020bd5b822058bb88923aa60c972b5d6ad89
Rodrigo Troncoso <rod.tronco@gmail.com>
2023-01-30 15:42:41 -0300

    revert(client): resolution=2 only retina

--- packages/client/src/components/Entities/TiledMap/Water/Water.component.tsx
+++ packages/client/src/components/Entities/TiledMap/Water/Water.component.tsx
@@ -41,8 +41,10 @@ export class WaterFilter extends Filter {
     this.uniforms.normal = Texture.EMPTY;
     this.uniforms.displacement = Texture.EMPTY;
     this.autoFit = false;
-    this.resolution = 2;
     this.padding = 0;
+    if (this.isRetina()) {
+      this.resolution = 2;
+    }
   }
 
   isRetina() {
