8b2e5dff013f9a35968055da84eabaa0ea046b67
Rodrigo Troncoso <rod.tronco@gmail.com>
2023-01-30 15:47:33 -0300

    fix(client): final retina fix

--- packages/client/src/components/Entities/TiledMap/Water/Water.component.tsx
+++ packages/client/src/components/Entities/TiledMap/Water/Water.component.tsx
@@ -57,7 +57,7 @@ export class WaterFilter extends Filter {
             'only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)'
           ).matches)) ||
         (window.devicePixelRatio && window.devicePixelRatio >= 2)) &&
-      /(iPad|iPhone|iPod)/g.test(navigator.userAgent)
+      /(iPad|iPhone|iPod|Macintosh)/g.test(navigator.userAgent)
     );
   }
 
